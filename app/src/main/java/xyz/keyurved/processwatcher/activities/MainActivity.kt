6548527c8fe7f74cd54e11cad13be1a631efa764
Keyur Ved <vedkeyur@gmail.com>
2018-09-26 15:51:22 -0400

    Add notification on process exit

--- app/src/main/java/xyz/keyurved/processwatcher/activities/MainActivity.kt
+++ app/src/main/java/xyz/keyurved/processwatcher/activities/MainActivity.kt
@@ -13,6 +13,11 @@ import xyz.keyurved.processwatcher.adapters.MessageViewAdapter
 import xyz.keyurved.processwatcher.entities.Message
 import xyz.keyurved.processwatcher.entities.WebSocketMessageReceiver
 import xyz.keyurved.processwatcher.entities.WebSocketService
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,10 +26,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewManager: RecyclerView.LayoutManager
     private val messageDataset: ArrayList<Message> = ArrayList()
     private var serviceStarted: Boolean = false
+    private var notificationManager: NotificationManagerCompat? = null
+    private var notificationId = 0
+
+    companion object {
+        const val DEFAULT_CHANNEL_ID = "DEFAULT_CHANNEL"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
+        notificationManager = NotificationManagerCompat.from(this)
 
 
         viewManager = LinearLayoutManager(this)
@@ -47,6 +60,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(DEFAULT_CHANNEL_ID, name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+    }
 
     private fun startWebSocketService() {
         if (!serviceStarted) {
@@ -63,6 +89,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showNotification(title: String, message: String) {
+        val builder = NotificationCompat.Builder(this, DEFAULT_CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        if (notificationManager != null) {
+            notificationManager!!.notify(notificationId++, builder.build())
+        }
+    }
+
     fun updateUI(status: String, message: Message?) {
         if (status == WebSocketService.SOCKET_OPEN) {
             connected_textView.text =  getString(R.string.active)
@@ -72,6 +110,9 @@ class MainActivity : AppCompatActivity() {
             connected_textView.text =  getString(R.string.inactive)
             connected_textView.setTextColor(getColor(R.color.red))
         } else if (status == WebSocketService.MESSAGE && message != null) {
+            if (message.messageType == Message.MessageType.EXIT) {
+                showNotification(message.processName, message.message)
+            }
             messageDataset.add(0, message)
             viewAdapter.notifyDataSetChanged()
         }
